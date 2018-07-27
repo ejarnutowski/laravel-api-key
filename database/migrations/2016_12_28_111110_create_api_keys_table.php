13cd2fc8cea07c5e18034a3a1cdf900da47796ff
Hugo <hugo@rezserve.com>
2018-07-27 16:45:03 -0700

    update migration

--- database/migrations/2016_12_28_111110_create_api_keys_table.php
+++ database/migrations/2016_12_28_111110_create_api_keys_table.php
@@ -28,16 +28,22 @@ class CreateApiKeysTable extends Migration
         Schema::create('domais', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('api_key_id');
-            $table->foreign('api_key_id')->references('id')->on('api_keys');
             $table->string('limiter');
             $table->enum('limiter_type', ['domain', 'ip','android_apps','ios_apps']);
             $table->unsignedInteger('number_request');
             $table->enum('status', ['active', 'inactive'])->default('active');
-            $table->timestamps();
+            $table->dateTime('created_at')->nullable()->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->dateTime('updated_at')->nullable()->default(\DB::raw('CURRENT_TIMESTAMP'));
             $table->softDeletes();
 
             $table->index('api_key_id');
             $table->index('limiter');
+
+            
+
+            // foreign key block
+            $table->foreign('api_key_id')
+                  ->references('id')->on('api_keys');
         });
     }
 
