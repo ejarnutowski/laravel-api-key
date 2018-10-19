425e12d73b08000b2261d135f06f86367b7d4dac
Kim Henriksen <kh@ipimp.at>
2018-10-19 20:23:23 +0200

    Added the word "numbers" to the invalid api key name message

--- src/Console/Commands/GenerateApiKey.php
+++ src/Console/Commands/GenerateApiKey.php
@@ -10,7 +10,7 @@ class GenerateApiKey extends Command
     /**
      * Error messages
      */
-    const MESSAGE_ERROR_INVALID_NAME_FORMAT = 'Invalid name.  Must be a lowercase alphabetic characters and hyphens less than 255 characters long.';
+    const MESSAGE_ERROR_INVALID_NAME_FORMAT = 'Invalid name.  Must be a lowercase alphabetic characters, numbers and hyphens less than 255 characters long.';
     const MESSAGE_ERROR_NAME_ALREADY_USED   = 'Name is unavailable.';
 
     /**
