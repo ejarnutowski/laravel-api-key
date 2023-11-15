eafeb994dab801f9df3ddc2f2e62f6ba24535be6
Sam Lee <cable8mm@gmail.com>
2023-11-15 20:15:58 +1300

    enable test framework and github actions

---
+++ tests/AlwaysTrueTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cable8mm\LaravelApiKey\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class AlwaysTrueTest extends TestCase
+{
+    public function test_always_true()
+    {
+        $this->assertTrue(true);
+    }
+}
